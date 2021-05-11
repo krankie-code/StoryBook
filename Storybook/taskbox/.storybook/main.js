7d104b1b127767ccb69817e3e7f20bb8d043c7c6
ferran.fores <ferran.fores@tekmaneducation.com>
2021-05-11 19:27:41 +0200

    storybook

---
+++ Storybook/taskbox/.storybook/main.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "stories": [
+    '../src/app/components/task.stories.ts',
+    '../src/app/components/task-list.stories.ts'
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
+}
